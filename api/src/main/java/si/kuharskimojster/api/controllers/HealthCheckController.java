a9cc7a2c51ac23976e31e44a084bf7eaf2db0abe
Jan Lampič <jan.lampic13@gmail.com>
2020-01-12 12:01:11 +0100

    added a healthcheck

---
+++ api/src/main/java/si/kuharskimojster/api/controllers/HealthCheckController.java
@@ -0,0 +1,16 @@
+package si.kuharskimojster.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import si.kuharskimojster.api.model.ResponseModel;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<ResponseModel> getHealth() {
+        return new ResponseEntity<>(new ResponseModel("Recommendation app is running ...", HttpStatus.OK.value()), HttpStatus.OK);
+    }
+}
